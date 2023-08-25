6c1eff049455334b62a28d0e60ea341fe81cf697
SUJITH S <113872741+sujith249@users.noreply.github.com>
2023-08-25 11:34:40 +0530

    Create 15.Strassen’s Matrix Multiplication.c

---
+++ 15.Strassen’s Matrix Multiplication.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void strassenMatrixMultiplication(int mat1[][2], int mat2[][2], int result[][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < 2; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int mat1[2][2] = {{1, 2}, {3, 4}};
+    int mat2[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2];
+    
+    strassenMatrixMultiplication(mat1, mat2, result);
+    
+    printf("Resultant matrix:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
