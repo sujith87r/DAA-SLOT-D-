c01db18404b304cd47c2baaf92cb0d2330e272dd
SUJITH S <113872741+sujith249@users.noreply.github.com>
2023-08-25 11:23:23 +0530

    Create 27.Floyd's Algorithm.c

---
+++ 27.Floyd's Algorithm.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define V 4
+#define INF 99999
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V];
+    
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+    
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+}
