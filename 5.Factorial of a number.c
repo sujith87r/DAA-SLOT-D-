94c0da41645fe6acd8c929f3c510bab6992156db
SUJITH S <113872741+sujith249@users.noreply.github.com>
2023-08-25 11:29:26 +0530

    Create 5.Factorial of a number.c

---
+++ 5.Factorial of a number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    int result = factorial(num);
+    printf("Factorial of %d is %d\n", num, result);
+    
+    return 0;
+}
