3e884f4e99edacf32a78e166b2f9bb4f1d1da7b3
SUJITH S <113872741+sujith249@users.noreply.github.com>
2023-08-25 11:21:05 +0530

    Create 22.Binomial Coefficient using Dynamic Programming.c

---
+++ 22.Binomial Coefficient using Dynamic Programming.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int binomialCoeff(int n, int k) {
+    int C[n+1][k+1];
+    
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= min(i, k); j++) {
+            if (j == 0 || j == i)
+                C[i][j] = 1;
+            else
+                C[i][j] = C[i-1][j-1] + C[i-1][j];
+        }
+    }
+    return C[n][k];
+}
