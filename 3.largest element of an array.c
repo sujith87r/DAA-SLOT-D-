67215e5942455dc20d7eb0d99efcc1cc1aee0065
SUJITH S <113872741+sujith249@users.noreply.github.com>
2023-08-25 11:27:26 +0530

    Create 3.largest element of an array.c

---
+++ 3.largest element of an array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int findLargest(int arr[], int size) {
+    int largest = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int largest = findLargest(arr, size);
+    printf("The largest element in the array is: %d\n", largest);
+    
+    return 0;
+}
